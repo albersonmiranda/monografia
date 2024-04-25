7242e3f7130c13a705209c5841162813ccaf6ed6
Alberson Miranda <albersonmiranda@hotmail.com>
2024-04-25 00:25:33 -0300

    update dados e estilo

--- data-raw/helper.R
+++ data-raw/helper.R
@@ -5,6 +5,6 @@
 .i = function() parent.frame(2)$i[]
 # looks for X or .x to handle base and purrr functionals
 .n = function() {
-    env = parent.frame(2)
-    names(c(env$X, env$.x))[env$i[]]
+  env = parent.frame(2)
+  names(c(env$X, env$.x))[env$i[]]
 }
--- data-raw/rais.R
+++ data-raw/rais.R
@@ -1,95 +1,93 @@
-## code to prepare `rais` dataset
-
-# pacotes
-library(magrittr, include.only = "%>%")
+### code to prepare `rais` dataset ##
 
 # set Google BigQuery project
 basedosdados::set_billing_id("monografia-359922")
 
 # importando dados da Rais
-rais = basedosdados::bdplyr("br_me_rais.microdados_vinculos") %>%
-    # filtrando dados
-    dplyr::filter(
-        # Espírito Santo
-        sigla_uf == "ES" &
-        # maiores de 25 anos
-        idade >= 25 &
-        # sem vazios em remuneração
-        !is.na(valor_remuneracao_media) &
-        # sem vazios em raça/cor
-        !is.na(raca_cor) & raca_cor != "9" &
-        # sem vazios em sexo
-        !is.na(sexo) &
-        # sem vazios em ocupação
-        !is.na(cbo_2002) &
-        # valores positivos para remuneração
-        valor_remuneracao_media > 0
-    ) %>%
-    # definindo grau de instrução
-    dplyr::mutate(
-        grau_instrucao = ifelse(ano <= 2005, grau_instrucao_1985_2005, grau_instrucao_apos_2005)
-    ) %>%
-    # escolhendo colunas
-    dplyr::select(
-        ano,
-        id_municipio,
-        sexo,
-        raca_cor,
-        valor_remuneracao_media,
-        idade,
-        grau_instrucao,
-        cbo_2002) %>%
-    # grau de instrução preenchido
-    dplyr::filter(!is.na(grau_instrucao)) %>%
-    # coletando dados
-    basedosdados::bd_collect()
+rais = basedosdados::bdplyr("br_me_rais.microdados_vinculos") |>
+  # filtrando dados
+  dplyr::filter(
+    # Espírito Santo
+    sigla_uf == "ES" &
+      # maiores de 25 anos
+      idade >= 25 &
+      # sem vazios em remuneração
+      !is.na(valor_remuneracao_media) &
+      # sem vazios em raça/cor
+      !is.na(raca_cor) & raca_cor != "9" &
+      # sem vazios em sexo
+      !is.na(sexo) &
+      # sem vazios em ocupação
+      !is.na(cbo_2002) &
+      # valores positivos para remuneração
+      valor_remuneracao_media > 0
+  ) |>
+  # definindo grau de instrução
+  dplyr::mutate(
+    grau_instrucao = ifelse(ano <= 2005, grau_instrucao_1985_2005, grau_instrucao_apos_2005)
+  ) |>
+  # escolhendo colunas
+  dplyr::select(
+    ano,
+    id_municipio,
+    sexo,
+    raca_cor,
+    valor_remuneracao_media,
+    idade,
+    grau_instrucao,
+    cbo_2002
+  ) |>
+  # grau de instrução preenchido
+  dplyr::filter(!is.na(grau_instrucao)) |>
+  # coletando dados
+  basedosdados::bd_collect()
 
 # dicionário Rais
 rais = within(rais, {
-    grau_instrucao = ifelse(grau_instrucao == "1", "analfabeto", grau_instrucao)
-    grau_instrucao = ifelse(grau_instrucao == "2", "fund_I_incompleto", grau_instrucao)
-    grau_instrucao = ifelse(grau_instrucao == "3", "fund_I_completo", grau_instrucao)
-    grau_instrucao = ifelse(grau_instrucao == "4", "fund_II_incompleto", grau_instrucao)
-    grau_instrucao = ifelse(grau_instrucao == "5", "fund_II_completo", grau_instrucao)
-    grau_instrucao = ifelse(grau_instrucao == "6", "medio_incompleto", grau_instrucao)
-    grau_instrucao = ifelse(grau_instrucao == "7", "medio_completo", grau_instrucao)
-    grau_instrucao = ifelse(grau_instrucao == "8", "superior_incompleto", grau_instrucao)
-    grau_instrucao = ifelse(grau_instrucao == "9", "superior_completo", grau_instrucao)
-    grau_instrucao = ifelse(grau_instrucao == "10", "mestrado", grau_instrucao)
-    grau_instrucao = ifelse(grau_instrucao == "11", "doutorado", grau_instrucao)
-    sexo = ifelse(sexo == "1", "masculino", sexo)
-    sexo = ifelse(sexo == "2", "feminino", sexo)
-    raca_cor = ifelse(raca_cor == "1", "indigena", raca_cor)
-    raca_cor = ifelse(raca_cor == "2", "branca", raca_cor)
-    raca_cor = ifelse(raca_cor == "4", "preta", raca_cor)
-    raca_cor = ifelse(raca_cor == "6", "amarela", raca_cor)
-    raca_cor = ifelse(raca_cor == "8", "parda", raca_cor)
-    raca_cor = ifelse(raca_cor == "9", "nao_informado", raca_cor)
-    sexo = relevel(as.factor(sexo), "feminino")
-    raca_cor = relevel(as.factor(raca_cor), "preta")
-    cbo = relevel(as.factor(substr(cbo_2002, 1, 2)), "76")
-    cbo = cbo_2002
-    cbo_2002 = NULL
-    vlr_rem = valor_remuneracao_media
-    valor_remuneracao_media = NULL
-    regiao = ifelse(
-        id_municipio %in% c("3205309", "3205200", "3205002", "3201308"),
-        "gv",
-        "interior"
-    )
-    regiao = relevel(as.factor(regiao), "interior")
+  grau_instrucao = ifelse(grau_instrucao == "1", "analfabeto", grau_instrucao)
+  grau_instrucao = ifelse(grau_instrucao == "2", "fund_I_incompleto", grau_instrucao)
+  grau_instrucao = ifelse(grau_instrucao == "3", "fund_I_completo", grau_instrucao)
+  grau_instrucao = ifelse(grau_instrucao == "4", "fund_II_incompleto", grau_instrucao)
+  grau_instrucao = ifelse(grau_instrucao == "5", "fund_II_completo", grau_instrucao)
+  grau_instrucao = ifelse(grau_instrucao == "6", "medio_incompleto", grau_instrucao)
+  grau_instrucao = ifelse(grau_instrucao == "7", "medio_completo", grau_instrucao)
+  grau_instrucao = ifelse(grau_instrucao == "8", "superior_incompleto", grau_instrucao)
+  grau_instrucao = ifelse(grau_instrucao == "9", "superior_completo", grau_instrucao)
+  grau_instrucao = ifelse(grau_instrucao == "10", "mestrado", grau_instrucao)
+  grau_instrucao = ifelse(grau_instrucao == "11", "doutorado", grau_instrucao)
+  sexo = ifelse(sexo == "1", "masculino", sexo)
+  sexo = ifelse(sexo == "2", "feminino", sexo)
+  raca_cor = ifelse(raca_cor == "1", "indigena", raca_cor)
+  raca_cor = ifelse(raca_cor == "2", "branca", raca_cor)
+  raca_cor = ifelse(raca_cor == "4", "preta", raca_cor)
+  raca_cor = ifelse(raca_cor == "6", "amarela", raca_cor)
+  raca_cor = ifelse(raca_cor == "8", "parda", raca_cor)
+  raca_cor = ifelse(raca_cor == "9", "nao_informado", raca_cor)
+  sexo = relevel(as.factor(sexo), "feminino")
+  raca_cor = relevel(as.factor(raca_cor), "preta")
+  cbo = relevel(as.factor(substr(cbo_2002, 1, 2)), "76")
+  cbo = cbo_2002
+  cbo_2002 = NULL
+  vlr_rem = valor_remuneracao_media
+  valor_remuneracao_media = NULL
+  regiao = ifelse(
+    id_municipio %in% c("3205309", "3205200", "3205002", "3201308"),
+    "gv",
+    "interior"
+  )
+  regiao = relevel(as.factor(regiao), "interior")
 })
 
 # agregando em ciclos completos
 rais = within(rais, {
-    grau_instrucao = ifelse(grau_instrucao %in% c("analfabeto", "fund_I_incompleto"), "nenhum", grau_instrucao)
-    grau_instrucao = ifelse(grau_instrucao %in% c("fund_II_incompleto", "fund_I_completo"), "fund_I", grau_instrucao)
-    grau_instrucao = ifelse(grau_instrucao %in% c("fund_II_completo", "medio_incompleto"), "fund_II", grau_instrucao)
-    grau_instrucao = ifelse(grau_instrucao %in% c("medio_completo", "superior_incompleto"), "medio", grau_instrucao)
-    grau_instrucao = ifelse(grau_instrucao %in% c("superior_completo"), "superior", grau_instrucao)
-    grau = relevel(as.factor(grau_instrucao), "nenhum")
-    grau_instrucao = NULL
+  grau_instrucao = ifelse(grau_instrucao %in% c("analfabeto", "fund_I_incompleto"), "nenhum", grau_instrucao)
+  grau_instrucao = ifelse(grau_instrucao %in% c("fund_II_incompleto", "fund_I_completo"), "fund_I", grau_instrucao)
+  grau_instrucao = ifelse(grau_instrucao %in% c("fund_II_completo", "medio_incompleto"), "fund_II", grau_instrucao)
+  grau_instrucao = ifelse(grau_instrucao %in% c("medio_completo", "superior_incompleto"), "medio", grau_instrucao)
+  grau_instrucao = ifelse(grau_instrucao %in% c("superior_completo"), "superior", grau_instrucao)
+  grau = relevel(as.factor(grau_instrucao), "nenhum")
+  grau_instrucao = NULL
 })
 
 # salvando dataframe
-saveRDS(rais, "data/rais.RDS", compress = FALSE)
+saveRDS(rais, "data/rais.RDS", compress = TRUE)
